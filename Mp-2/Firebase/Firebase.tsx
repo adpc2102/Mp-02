079ef0e1646a8a9779a4ebf25102d1dac4a4cf57
adpc2102 <andrespaz2002@gmail.com>
2024-03-09 18:26:24 -0400

    MP-02

---
+++ Mp-2/Firebase/Firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAb3jBtBb39shGC-3y-BuS9bAPI0-o0OVY",
+  authDomain: "mp-2-e6a16.firebaseapp.com",
+  projectId: "mp-2-e6a16",
+  storageBucket: "mp-2-e6a16.appspot.com",
+  messagingSenderId: "399296775315",
+  appId: "1:399296775315:web:c9fd911502eed470257966"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
